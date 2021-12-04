b4eb906ce234f51bfbacde1d8bdf6057aff4803c
totally-not-horny <dacentvova@gmail.com>
2021-12-04 08:56:03 +0400

    Initial commit

---
+++ Geoculus/Geoculus.Android/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Gms.Common;
+using Android.Runtime;
+using Android.OS;
+using Android.Util;
+using Firebase.Iid;
+
+namespace Geoculus.Droid
+{
+    [Activity(Label = "Geoculus", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            CreateNotificationChannel();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+       
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                "FCM Notifications",
+                NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+            var str = FirebaseInstanceId.Instance.Token;
+            //Log.Debug("INFO", str);
+        }
+    }
+}
---
+++ Geoculus/Geoculus/MainPage.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace Geoculus
+{
+
+    public partial class MainPage : ContentPage
+    {
+        private class ResultsClass
+        {
+            public List<ElevationInfo> Results { get; set; }
+        }
+
+        private class ElevationInfo
+        {
+            public double Latitude { get; set; }
+            public double Longtitude { get; set; }
+            public double Elevation { get; set; }
+        }
+        private int Freq;
+        private int Frame;
+        private List<double> xS;
+        private Polyline route;
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            xS = new List<double>();
+            if (OrientationSensor.IsMonitoring)
+                ;
+            else
+                OrientationSensor.Start(SensorSpeed.Default);
+            if (Accelerometer.IsMonitoring)
+                ;
+            else
+                Accelerometer.Start(SensorSpeed.Default);
+            if (Compass.IsMonitoring)
+                ;
+            else
+                Compass.Start(SensorSpeed.Default);
+            Compass.ReadingChanged += Compass_ReadingChanged;
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            OrientationSensor.ReadingChanged += OrientationSensor_ReadingChanged;
+            Device.StartTimer(TimeSpan.FromSeconds(10), Callback );
+        }
+
+        private void Barometer_ReadingChanged(object sender, BarometerChangedEventArgs e)
+        {
+            Butt.Text = e.Reading.PressureInHectopascals.ToString();
+        }
+
+        private bool Callback()
+        {
+
+            UpdateLocation();
+            return true;
+        }
+
+        private async Task UpdateLocation()
+        {
+            var l = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best));
+            
+            l.AltitudeReferenceSystem = AltitudeReferenceSystem.Geoid;
+            if (route == null)
+            {
+                route = new Polyline();
+                map.MapElements.Add(route);
+
+            }
+             
+            if (route.Geopath.Count() > 2)
+            {
+                var distance = LocationExtensions.CalculateDistance(l, route.Geopath.Last().Latitude,
+                    route.Geopath.Last().Longitude, DistanceUnits.Kilometers) * 1000;
+                if (distance > 30)
+                {
+                    l.Latitude = route.Geopath.Last().Latitude +
+                                 30d / distance * (l.Latitude - route.Geopath.Last().Latitude);
+                    l.Longitude = route.Geopath.Last().Longitude +
+                                  30d / distance * (l.Longitude - route.Geopath.Last().Longitude);
+                }
+            }
+
+            route.Geopath.Add(new Position(l.Latitude, l.Longitude));
+            Gay.Text = l.ToString();
+            Butt.Text = l.Altitude.ToString();
+
+            map.MapElements.Add(new Circle(){Center = new Position(l.Latitude, l.Longitude), Radius = new Distance(l.Accuracy.Value)});
+            map.MoveToRegion(new MapSpan(new Position(l.Latitude, l.Longitude), 0.001, 0.001));
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.RequestUri = new Uri($"https://api.open-elevation.com/api/v1/lookup?locations={l.Latitude.ToString("F6", CultureInfo.InvariantCulture)},{l.Longitude.ToString("F6", CultureInfo.InvariantCulture)}");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accept", "application/json");
+            var response = await client.SendAsync(request);
+            var str = await response.Content.ReadAsStringAsync();
+            
+            
+            
+            var elevation = JsonConvert.DeserializeObject<ResultsClass>(str).Results[0].Elevation;
+
+            if (l.Altitude-16 - elevation > 6)
+            {
+                Gay.ForegroundColor = Color.Red;
+            }
+            else
+            {
+                Gay.ForegroundColor = Color.White;
+
+            }
+            HttpClient client2 = new HttpClient();
+            HttpRequestMessage request2 = new HttpRequestMessage();
+            try
+            {
+                request2.RequestUri = new Uri(
+                    $"http://192.168.159.50:4444/send?lat={l.Latitude.ToString("F6", CultureInfo.InvariantCulture)}&lon={l.Longitude.ToString("F6", CultureInfo.InvariantCulture)}&elev={((l.Altitude.HasValue? l.Altitude.Value:16) - 16.0).ToString("F6", CultureInfo.InvariantCulture)}");
+
+
+            
+
+            request2.Method = HttpMethod.Get;
+            var res = await client2.SendAsync(request2);
+            }
+            catch (Exception e)
+            {
+
+            }
+            Butt.Text = l.Altitude.ToString() + " " + elevation;
+        }
+
+        private void OrientationSensor_ReadingChanged(object sender, OrientationSensorChangedEventArgs e)
+        {
+            var data = e.Reading.Orientation;
+            double x, y, z = 0;
+            var coef = Math.Sin(Math.Acos(data.W));
+            xS.Add(data.X/coef);
+            var test = xS;
+            x = data.X ;
+            y = data.Y ;
+            z = data.Z ;
+            Trans.Text = $"X:{x:F3} Y:{y:F3} Z:{z:F3}";
+        }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            Suck.Text = "";
+        }
+
+        private async void MainPage_OnAppearing(object sender, EventArgs e)
+        {
+            var l = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best));
+
+            
+            var placemark = new Placemark();
+            placemark.Location = l;
+            
+            Gay.Text = placemark.ToString();
+        }
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            var data = e.Reading;
+            Ass.Text = data.HeadingMagneticNorth.ToString() + "\n";
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await UpdateLocation();
+        }
+    }
+
+}
